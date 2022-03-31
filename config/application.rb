42033d6725bdf1f87a694391cd7e47872df92f71
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-03-31 16:39:33 +0900

    rails g scaffold article title:string body:textコマンドを実行。

--- config/application.rb
+++ config/application.rb
@@ -2,10 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
-
 module HelloWorldRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -15,11 +11,15 @@ module HelloWorldRails
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
     config.generators do |g|
       g.javascripts false
       g.stylesheets false
       g.helper false
       g.test_framework false
+      g.template_engine false
     end
+
+    config.api_only = true
   end
 end
