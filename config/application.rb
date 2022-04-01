30e42cde47fd6cae3c004d06f821ddb7a99def1e
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-04-02 00:51:30 +0900

    generatorの挙動の修正

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,10 @@
 require_relative 'boot'
 
 require 'rails/all'
+Bundler.require(*Rails.groups)
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
 
 module HelloWorldRails
   class Application < Rails::Application
@@ -15,11 +15,14 @@ module HelloWorldRails
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
     config.generators do |g|
       g.javascripts false
       g.stylesheets false
       g.helper false
       g.test_framework false
+      g.template_engine false
     end
+    config.api_only = true
   end
 end
