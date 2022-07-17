6d65734238b623924e1d87c935dcbcf21cf8680b
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-07-17 23:01:13 +0900

    users_controller の index メソッドのテストを実装

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
+    subject { get(users_path) }
+    before { create_list(:user,3) }
+
     it "ユーザーの一覧が取得できる" do
-      get users_path
-      expect(response).to have_http_status(200)
-    end
-  end
+      subject
+
+     res = JSON.parse(response.body)
+     expect(res.length).to eq 3
+     expect(res[0].keys).to eq ["account", "name", "email"]
+     expect(response).to have_http_status(200)
+     end
+   end
 
   describe "GET /users/:id" do
     it "任意のユーザーのレコードが取得できる" do
@@ -19,14 +26,12 @@ RSpec.describe "Users", type: :request do
   end
 
   describe "PUT /users/:id" do
-    it "ユーザーのレコードを更新できる"do
+    it "ユーザーのレコードを更新できる" do
     end
   end
-
 
   describe "DELETE /users/:id" do
     it "任意のユーザーのレコードを削除できる" do
     end
    end
-
-end
+  end
